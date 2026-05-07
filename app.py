a99fe95762c3486db8e97cc391cf2d37009fadf1
luis-fernando-chambi-orosco <104604119+luis-fernando-chambi-orosco@users.noreply.github.com>
2026-05-06 19:59:54 -0500

    final

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def index():
             if corr_id is None:
                 respuesta = "No se pudo enviar la solicitud RPC."
             else:
-                timeout = 30
+                timeout = 60
                 elapsed = 0
                 while not RPC_CLIENT.has_response(corr_id):
                     sleep(0.1)
